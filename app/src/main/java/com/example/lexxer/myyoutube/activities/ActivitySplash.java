e378a941f13217bfca8f4bd1b2e389fe292d8d08
LeXXeR <lex1977@gmail.com>
2016-06-05 14:10:04 +0600

    first commit

---
+++ app/src/main/java/com/example/lexxer/myyoutube/activities/ActivitySplash.java
@@ -0,0 +1,61 @@
+package com.example.lexxer.myyoutube.activities;
+
+        import android.content.Intent;
+        import android.os.AsyncTask;
+        import android.os.Build;
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.view.View;
+        import android.view.Window;
+        import android.view.WindowManager;
+
+        import com.example.lexxer.myyoutube.R;
+
+public class ActivitySplash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Loading().execute();
+    }
+
+    public class Loading extends AsyncTask<Void, Void, Void>{
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            try {
+                Thread.sleep(2000);
+            }catch (InterruptedException ie){
+                ie.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            Intent intent = new Intent(getApplicationContext(), ActivityHome.class);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            if (hasFocus) {
+                getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/lexxer/myyoutube/activities/test.java
@@ -0,0 +1,9 @@
+package com.example.lexxer.myyoutube.activities;
+
+/**
+ * Created by LeXXeR on 02.06.2016.
+ */
+public class test {
+
+
+}
